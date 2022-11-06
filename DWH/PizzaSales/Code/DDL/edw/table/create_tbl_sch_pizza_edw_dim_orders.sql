e63e9a4767c66ab659848f8b4fa9612cd16e614c
Nitin Raj Srivastava <96106142+nitindwhbi@users.noreply.github.com>
2022-11-06 16:21:11 +0100

    added additional column from staging table

--- DWH/PizzaSales/Code/DDL/edw/table/create_tbl_sch_pizza_edw_dim_orders.sql
+++ DWH/PizzaSales/Code/DDL/edw/table/create_tbl_sch_pizza_edw_dim_orders.sql
@@ -5,6 +5,8 @@ create table sch_pizza_edw.dim_orders
     order_sk integer primary key not null,
     order_id integer,
     order_details_id integer,
+    product_id varchar(100),
+    quantity smallint,
     date date,
     time time,
     rec_source varchar(10),
